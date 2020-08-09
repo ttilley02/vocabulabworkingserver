e48a30d8cb514d14a9ddfa6512656036132b335c
ttilley02 <ttilley02@gmail.com>
2020-08-08 23:25:23 -0400

    delete

--- src/notes/notes-service.js
+++ src/notes/notes-service.js
@@ -55,10 +55,7 @@ const notesService = {
 
 
   deleteNote(db, id, user) {
-  return db
-  .from('vocabulab_notes AS notes')
-  .where({card_id: id, user_id: user})
-  .delete();
+    db.raw(`delete from vocabulab_notes where card_id = 1 and user_id = 3` )
 },
 
    
