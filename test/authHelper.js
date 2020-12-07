90e624dced0d06d6b2c2e98f21a71b8572847f2c
ttilley02 <ttilley02@gmail.com>
2020-12-07 08:00:03 -0500

    misc and more notes

--- test/authHelper.js
+++ test/authHelper.js
@@ -1,14 +1,16 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+//test auth to be used in spec.js
+
 function makeAuthHeader(user, secret = process.env.JWT_SECRET) {
-  const token = jwt.sign({ user_id: user.id }, secret, {
-    subject: user.user_name,
-    algorithm: "HS256"
-  });
-  return `Bearer ${token}`;
+   const token = jwt.sign({ user_id: user.id }, secret, {
+      subject: user.user_name,
+      algorithm: "HS256",
+   });
+   return `Bearer ${token}`;
 }
 
 module.exports = {
-  makeAuthHeader
+   makeAuthHeader,
 };
--- test/fixtures/user.fixtures.js
+++ test/fixtures/user.fixtures.js
@@ -1,26 +1,27 @@
 function makeUsersArray() {
-  return [
-    {
-      id: 1,
-      user_name: "testperson1",
-      full_name: "Caesar Salad",
-      password: "password",
-      nickname: "TT",
-      date_created: "2020-08-08 18:26:51",
-      date_modified: "2020-08-08 18:26:51"
-    },
-    {
-      id: 2,
-      user_name: "testperson2",
-      full_name: "Joey Jammy",
-      password: "password",
-      nickname: "TT",
-      date_created: "2020-08-08 18:26:51",
-      date_modified: "2020-08-08 18:26:51"
-    }
-  ];
+   //testing tools:dummy entries for test database
+   return [
+      {
+         id: 1,
+         user_name: "testperson1",
+         full_name: "Caesar Salad",
+         password: "password",
+         nickname: "TT",
+         date_created: "2020-08-08 18:26:51",
+         date_modified: "2020-08-08 18:26:51",
+      },
+      {
+         id: 2,
+         user_name: "testperson2",
+         full_name: "Joey Jammy",
+         password: "password",
+         nickname: "TT",
+         date_created: "2020-08-08 18:26:51",
+         date_modified: "2020-08-08 18:26:51",
+      },
+   ];
 }
 
 module.exports = {
-  makeUsersArray
+   makeUsersArray,
 };
