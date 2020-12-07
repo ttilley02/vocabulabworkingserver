e54ad4698fb5dbe88c056ff554031c8ccfd5ccc9
ttilley02 <ttilley02@gmail.com>
2020-12-07 08:02:54 -0500

    misc and more notes

--- src/notes/notes-service.js
+++ src/notes/notes-service.js
@@ -1,19 +1,20 @@
 const xss = require("xss");
 
+//KNEX code for SQL scripts for database
 const notesService = {
-  getById(db, id) {
-    return db
-      .from("vocabulab_notes AS note")
-      .select(
-        "note.note",
-        "note.carddata",
-        "note.favorite",
-        "note.known",
-        "note.date_created",
-        "note.card_id",
-        "note.user_id",
-        db.raw(
-          `json_strip_nulls(
+   getById(db, id) {
+      return db
+         .from("vocabulab_notes AS note")
+         .select(
+            "note.note",
+            "note.carddata",
+            "note.favorite",
+            "note.known",
+            "note.date_created",
+            "note.card_id",
+            "note.user_id",
+            db.raw(
+               `json_strip_nulls(
             row_to_json(
               (SELECT tmp FROM (
                 SELECT
@@ -26,51 +27,51 @@ const notesService = {
               ) tmp)
             )
           ) AS "user"`
-        )
-      )
-      .leftJoin("vocabulab_users AS usr", "note.user_id", "usr.id")
-      .first();
-  },
+            )
+         )
+         .leftJoin("vocabulab_users AS usr", "note.user_id", "usr.id")
+         .first();
+   },
 
-  insertNote(db, newNote) {
-    return db
-      .insert(newNote)
-      .into("vocabulab_notes")
-      .returning("*")
-      .then(([note]) => note)
-      .then((note) => notesService.getById(db, note.id));
-  },
+   insertNote(db, newNote) {
+      return db
+         .insert(newNote)
+         .into("vocabulab_notes")
+         .returning("*")
+         .then(([note]) => note)
+         .then((note) => notesService.getById(db, note.id));
+   },
 
-  updateNote(db, user_id, card_id, newNoteFields) {
-    console.log("Service object", user_id, card_id);
-    return db("vocabulab_notes")
-      .where({ user_id, card_id })
-      .update(newNoteFields);
-  },
+   updateNote(db, user_id, card_id, newNoteFields) {
+      console.log("Service object", user_id, card_id);
+      return db("vocabulab_notes")
+         .where({ user_id, card_id })
+         .update(newNoteFields);
+   },
 
-  deleteNote(db, card_id, user_id) {
-    return db.from("vocabulab_notes").where({ card_id, user_id }).delete();
-  },
+   deleteNote(db, card_id, user_id) {
+      return db.from("vocabulab_notes").where({ card_id, user_id }).delete();
+   },
 
-  serializeNote(note) {
-    const { user } = note;
-    return {
-      note: note.note,
-      carddata: note.carddata,
-      known: note.known,
-      favorite: note.favorite,
-      date_created: new Date(note.date_created),
-      card_id: note.card_id,
-      user: {
-        id: user.id,
-        user_name: user.user_name,
-        full_name: user.full_name,
-        nickname: user.nickname,
-        date_created: new Date(user.date_created),
-        date_modified: new Date(user.date_modified) || null
-      }
-    };
-  }
+   serializeNote(note) {
+      const { user } = note;
+      return {
+         note: note.note,
+         carddata: note.carddata,
+         known: note.known,
+         favorite: note.favorite,
+         date_created: new Date(note.date_created),
+         card_id: note.card_id,
+         user: {
+            id: user.id,
+            user_name: user.user_name,
+            full_name: user.full_name,
+            nickname: user.nickname,
+            date_created: new Date(user.date_created),
+            date_modified: new Date(user.date_modified) || null,
+         },
+      };
+   },
 };
 
 module.exports = notesService;
--- test/fixtures/cards.fixtures.js
+++ test/fixtures/cards.fixtures.js
@@ -1,29 +1,30 @@
 function makeCardsArray() {
-  return [
-    {
-      id: 1,
-      spa_content: "spanish stuff",
-      eng_content: "english stuff",
-      difficulty: "b",
-      user_id: null
-    },
-    {
-      id: 2,
-      spa_content: "spanish stuff2",
-      eng_content: "english stuff2",
-      difficulty: "b",
-      user_id: null
-    },
-    {
-      id: 3,
-      spa_content: "spanish stuff3",
-      eng_content: "english stuff3",
-      difficulty: "b",
-      user_id: null
-    }
-  ];
+   //testing tools:dummy entries for test database
+   return [
+      {
+         id: 1,
+         spa_content: "spanish stuff",
+         eng_content: "english stuff",
+         difficulty: "b",
+         user_id: null,
+      },
+      {
+         id: 2,
+         spa_content: "spanish stuff2",
+         eng_content: "english stuff2",
+         difficulty: "b",
+         user_id: null,
+      },
+      {
+         id: 3,
+         spa_content: "spanish stuff3",
+         eng_content: "english stuff3",
+         difficulty: "b",
+         user_id: null,
+      },
+   ];
 }
 
 module.exports = {
-  makeCardsArray
+   makeCardsArray,
 };
