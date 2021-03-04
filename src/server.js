f511bab2ee01d1bca47db3723eee8e70ca000639
ttilley02 <56703674+ttilley02@users.noreply.github.com>
2021-03-04 08:46:20 -0500

    dependencies

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
-require("dotenv").config();
-
 const knex = require("knex");
 const app = require("./app");
 const { PORT, DATABASE_URL } = require("./config");
 
 const db = knex({
   client: "pg",
-  connection: DATABASE_URL
+  connection: DATABASE_URL,
 });
 
 app.set("db", db);
